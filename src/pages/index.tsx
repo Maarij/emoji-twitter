cb5791e114e3877041206a42349cb7ee61e1aeaf
Maarij <maarij.anwar@gmail.com>
2023-11-05 08:56:37 -0600

    attach user auth to trpc context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
