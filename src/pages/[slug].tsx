ea96da418f4c0bd8f6ef7251ec7f818cf6532336
Maarij <maarij.anwar@gmail.com>
2023-11-10 19:45:24 -0600

    profile page design

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -16,14 +16,28 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
       </Head>
 
       <PageLayout>
-        <div>{data.username}</div>
+        <div className="relative h-36  bg-slate-600">
+          <Image
+            src={data.imageUrl}
+            alt={`${data.username ?? ""}'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">
+          {`@${data.username}` ?? ""}
+        </div>
+        <div className="border-b border-slate-400"></div>
       </PageLayout>
     </>
   );
 };
 
-import { generateServerSideHelpers } from "~/server/helpers/ssgHelper";
+import Image from "next/image";
 import { PageLayout } from "~/components/pageLayout";
+import { generateServerSideHelpers } from "~/server/helpers/ssgHelper";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const helpers = generateServerSideHelpers();
